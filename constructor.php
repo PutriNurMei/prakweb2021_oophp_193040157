9155139c1c2aaa2f111750de6d633875a9e69b3a
Putri Nurmeida Mayasari <63586977+PutriNurMei@users.noreply.github.com>
2021-09-17 19:39:40 +0700

    membuat konsep constructor pada sebuah class Produk,  yang dimana constructor merupakan sebuah method yang khusus yang ada dalam sebuah class, dan constructor adalah method yang otomatis dijalankan ketika sebuah class di instansiasi

---
+++ constructor.php
@@ -0,0 +1,43 @@
+<?php
+
+class Produk
+{
+
+  public  $judul,
+    $penulis,
+    $penerbit,
+    $harga;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "peenrbit", $harga = 0)
+  {
+    $this->judul = $judul;
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+    //echo "Hello World!";
+  }
+
+
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
+}
+
+
+
+
+
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 50000);
+
+
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 5000000);
+
+$produk3 = new Produk("Dragon Ball");
+
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
+echo "<br>";
+var_dump($produk3);
