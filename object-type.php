95bf6718b39269100a87111332d90c5bf62f4470
Putri Nurmeida Mayasari <63586977+PutriNurMei@users.noreply.github.com>
2021-09-17 22:10:03 +0700

    membuat object data dari salah satu type data yang bisa digunakan pada class. dan pada vidio 5 ini membahas dan mengenalkan type data pada php

---
+++ object-type.php
@@ -0,0 +1,54 @@
+<?php
+
+class Produk
+{
+
+  public  $judul,
+    $penulis,
+    $penerbit,
+    $harga;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "peenrbit", $harga = 0)
+  {
+    $this->judul = $judul;
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+    //echo "Hello World!";
+  }
+
+
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
+}
+
+class cetakInfoProduk
+{
+  public function cetak($produk)
+  {
+    $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+    return $str;
+  }
+}
+
+
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 50000);
+
+
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 5000000);
+
+//$produk3 = new Produk("Dragon Ball");
+
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
+//echo "<br>";
+//var_dump($produk3);
+
+echo "<br>";
+
+$infoProduk1 = new cetakInfoProduk();
+echo $infoProduk1->cetak($produk1);
