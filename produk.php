09ed0a0ba7065fa7e41cd1348e558227abce74bf
Putri Nurmeida Mayasari <63586977+PutriNurMei@users.noreply.github.com>
2021-09-17 17:44:02 +0700

    membuat class produk.php dan berisi property&method, property yaitu sesuatu yang mempresentasikan data/keadaan dari sebuah object dan method yaitu sesuatu yang mempresentasikan prilaku dari sebuah objek, yang di pelajari pada vidio 3

---
+++ produk.php
@@ -0,0 +1,54 @@
+<?php
+
+class Produk
+{
+
+  public  $judul = 'judul',
+    $penulis = 'penulis',
+    $penerbit = 'penerbit',
+    $harga = 0;
+
+  //public function sayHello() {
+  //return "Hello Word";
+  //}
+
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
+}
+
+
+//$produk1 = new Produk();
+//$produk1->judul = "Naruto";
+//var_dump($produk1);
+
+//$produk2 = new Produk();
+//$produk2->judul = "Uncharted";
+//$produk2->tambahProperty = "hahaha"
+//var_dump($produk2);
+
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 50000;
+//var_dump($produk3);
+
+//echo "Komik : $produk3->penulis, $produk3->penerbit";
+//echo "<br>";
+//echo $produk3->sayHello();
+//echo $produk3->getLabel();
+
+//echo "<hr>";
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 5000000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
